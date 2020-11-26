f727312b665c532957a8eca158f589e6d9276f34
Sandeep <sramanathan@tesla.com>
2020-11-26 12:19:05 -0800

    Pushing calculator library with Nuget package

---
+++ Calculator/SimpleCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public SimpleCalculator() { }
+
+        public double Calculate (double a, double b, String ops)
+        {
+            switch (ops)
+            {
+                case "add":
+                    return a + b;
+                case "subtract":
+                    return a - b;
+                case "multiply":
+                    return a * b;
+                case "divide":
+                    if (b == 0)
+                    {
+                        throw new ArithmeticException();
+                    }
+                    return a / b;
+                default:
+                    throw new ArithmeticException();
+            }
+        }
+    }
+}
